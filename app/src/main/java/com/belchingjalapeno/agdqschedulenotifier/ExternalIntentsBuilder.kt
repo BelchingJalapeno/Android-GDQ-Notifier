bffe06d0b218210a42dd00a26e24e684c17c555f
BelchingJalapeno <BelchingJalapeno@gmail.com>
2018-06-27 15:01:48 -0500

    change default donate url

--- app/src/main/java/com/belchingjalapeno/agdqschedulenotifier/ExternalIntentsBuilder.kt
+++ app/src/main/java/com/belchingjalapeno/agdqschedulenotifier/ExternalIntentsBuilder.kt
@@ -20,7 +20,7 @@ class ExternalIntentsBuilder {
         }
 
         fun getDonateIntent(sharedPreferences: SharedPreferences): Intent {
-            return Intent(Intent.ACTION_VIEW, Uri.parse(sharedPreferences.getString(DONATE_PREFERENCE_KEY, "https://gamesdonequick.com/tracker/donate/sgdq2018")))
+            return Intent(Intent.ACTION_VIEW, Uri.parse(sharedPreferences.getString(DONATE_PREFERENCE_KEY, "https://gamesdonequick.com/tracker/ui/donate/sgdq2018")))
         }
 
         fun getFilePickerJsonIntent(): Intent {
