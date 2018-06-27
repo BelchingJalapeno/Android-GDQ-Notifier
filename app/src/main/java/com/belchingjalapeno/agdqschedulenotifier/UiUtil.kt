dc4cc05c8a3f8bea19c0da922e1b21d9112d595a
BelchingJalapeno <BelchingJalapeno@gmail.com>
2018-06-27 14:37:12 -0500

    remove unused method in UiUtil convert anonymous class to lambda

--- app/src/main/java/com/belchingjalapeno/agdqschedulenotifier/UiUtil.kt
+++ app/src/main/java/com/belchingjalapeno/agdqschedulenotifier/UiUtil.kt
@@ -1,38 +1,16 @@
 package com.belchingjalapeno.agdqschedulenotifier
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ValueAnimator
 import android.support.v4.graphics.ColorUtils
 import android.view.View
 import android.widget.ImageView
 
-//https://stackoverflow.com/questions/4043398/animate-visibility-modes-gone-and-visible
-fun animateViewVisibility(view: View, visibility: Int) {
-    // cancel runnning animations and remove and listeners
-    view.animate().cancel()
-    view.animate().setListener(null)
-
-    // animate making view visible
-    if (visibility == View.VISIBLE) {
-        view.animate().alpha(1f).start()
-        view.visibility = View.VISIBLE
-    } else {
-        view.animate().setListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animation: Animator) {
-                view.visibility = visibility
-            }
-        }).alpha(0f).start()
-    }// animate making view hidden (HIDDEN or INVISIBLE)
-}
-
 fun addColorAnimation(imageView: ImageView, startColor: Int, endColor: Int) {
-    imageView.animate().setUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
-        override fun onAnimationUpdate(p0: ValueAnimator?) {
-            val percent = p0!!.currentPlayTime / p0!!.duration.toFloat()
+    imageView.animate().setUpdateListener { p0 ->
+        if (p0 != null) {
+            val percent = p0.currentPlayTime / p0.duration.toFloat()
             imageView.setColorFilter(ColorUtils.blendARGB(startColor, endColor, clamp(percent)))
         }
-    })
+    }
 }
 
 fun addAlphaAnimation(imageView: View, endAlpha: Float) {
